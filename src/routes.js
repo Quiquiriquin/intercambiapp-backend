ac18b5cfe6229b39e55507d2f55a7265b8753040
Enrique Alvarez <enrique.alvarez@gintelligence.mx>
2021-01-11 00:39:05 -0600

    Fixed routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import userRoutes from "./routes/UserRoute";
 
 export default (app) => {
-  app.use("/api/v1/", userRoutes);
+  app.use("api/v1/", userRoutes);
   // app.get("/api/v1/recipe", recipeController.getRecipes);
 };
